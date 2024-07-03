5827e9b3d613d81bc73385746a10cbec61d7b81f
Chullu1609 <165214813+Chullu1609@users.noreply.github.com>
2024-07-03 08:25:36 +0200

    Second commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//let message = "Hello World";
+//console.log(message);
+
+//let myName = "Chulumanco";
+//myName = "Chulumanco";
+//console.log("Hello " + myName);
+//if (myName.length > 7) {
+//    console.log("Wow, you have a REALLY long name!");
+//}else {
+//    console.log("Your name isn't very long.");
+//    }
+
+//let lemonChicken = false;
+//let beefWithBlackBean = true;
+//let sweetAndSourPork = true;
+//if (lemonChicken) {
+//console.log("Great! I'm having lemon chicken!");
+//} else if (beefWithBlackBean) {
+//console.log("I'm having the beef.");
+//} else if (sweetAndSourPork) {
+//console.log("OK, I'll have the pork.");
+//} else {
+//console.log("Well, I guess I'll have rice then.");
+//}
+
+//let sheepCounted = 0;
+//while (sheepCounted < 10) {
+//console.log("I have counted " + sheepCounted + " sheep!");
+//  sheepCounted++;
+//}
+//console.log("Zzzzzzzzzzz");
+
+let timesToSayHello = 3;
+for (let i = 0; i < timesToSayHello; i++) {
+console.log("Hello!");
+}
